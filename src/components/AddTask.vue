6d6e935bbed8bed700c1e5775e8d8087ca6e9e64
Daniel Silva <danifilipesilv@gmail.com>
2023-04-05 11:20:34 +0100

    Added checkbox structure and css

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -31,16 +31,19 @@
     .add-task{
         display: flex;
         flex-direction: row;
+        justify-content: space-between;
         align-items: center;
         background-color: var(--grey);
         padding: 0.3rem;
         margin: 1.3rem 0rem 0.5rem 0rem;
     }
     .name{
+        width: 11.5rem;
         padding: 0.5rem 0rem 0.5rem 0.4rem
     }
     .date{
-        padding: 0.5rem 0.2rem;
+        width: 6rem;
+        padding: 0.5rem 0.2rem 0.5rem 0.5rem;
     }
     .name, .date{
         border-radius: 0.5rem;
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="task radious-and-shadow">
-        <div class="task-name"> {{ thisTask.name }} </div>
+        <div class="check-and-name">
+            <input type="checkbox" class="checkbox remove-default">
+            <div class="task-name"> {{ thisTask.name }} </div>
+        </div>
         <div class="button-holder actions radious-and-shadow">
             <div class="date"> {{ thisTask.date }} </div>
             <button class="remove-default button radious-and-shadow"> Delete </button>
@@ -35,9 +38,28 @@
         background-color: var(--grey);
         margin: 0.4rem 0rem;
     }
+    .check-and-name{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+    .checkbox{
+        margin: 0.5rem;
+        padding: 0.6rem;
+        border-radius: 50%;
+        background-color: var(--darkGrey);
+        border: 0.05rem solid var(--blue)
+    }
+    .checkbox:hover{
+        background-color: var(--grey);
+        border-color: var(--hover);
+    }
+    .checkbox:checked{
+        background-color: var(--grey);
+        border-color: var(--darkestGrey);
+    }
     .task-name{
-        padding-left: 1rem;
-        
+        padding-left: 0.2rem;
     }
     .actions{
         margin: 0.4rem;
@@ -49,5 +71,15 @@
     }
     .date{
         margin-right: 0.5rem;
+    }
+    .task-name{
+        font-size: 1.1rem;
+    }
+    .task-name, .date{
+        font-weight: 500;
+    }
+    .task:has(.checkbox:checked){
+        opacity: 0.5;
+        text-decoration: line-through;
     }
 </style>
