e8213159877947434998eef257808966c4ae6e23
Daniel Silva <danifilipesilv@gmail.com>
2023-04-06 12:06:37 +0100

    Changes in lists flex

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -214,7 +214,6 @@
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
-    justify-content: center;
-    align-items: space-between;
+    justify-content: space-around;
   }
 </style>
