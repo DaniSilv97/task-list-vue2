a006a0ca61cb954ddc3fdd69c9b1ea0045e88738
Daniel Silva <danifilipesilv@gmail.com>
2023-04-06 14:20:44 +0100

    Color swatch, structure, css, and functionality

---
+++ src/components/Swatches.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="swatches button-holder radius-and-shadow" >
+        <div class="colors-container">
+            <div class="accent-colors">
+                <input type="color" disabled :value="thisSwatch.mainColor">
+                <input type="color" disabled :value="thisSwatch.secColor">
+            </div>
+            <div class="time-colors">
+                <input type="color" disabled :value="thisSwatch.enoughTime">
+                <input type="color" disabled :value="thisSwatch.shortTime">
+                <input type="color" disabled :value="thisSwatch.noTime">
+            </div>
+        </div>
+        <button class="button" @click="setTheme"> Swatch {{ thisSwatch.id }}</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [ 'thisSwatch' ],
+        components: {  }, 
+        name: 'Swatches',
+        data(){
+            return{
+                
+            }
+        },
+        methods:{
+            setTheme(){
+                    this.$emit('setTheme', this.thisSwatch)
+                }
+        },
+        computed:{
+        
+        },
+    }
+</script>
+
+<style scoped>
+    .swatches{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin: 0.5rem 0.2rem;
+    }
+    .colors-container{
+        background-color: var(--grey);
+        padding: 0.3rem;
+        flex: 1 1 0;
+        display: flex;
+        justify-content: space-between;
+        margin-right: 1rem;
+    }
+    input[type="color"]{
+        height: 3rem;
+        width: 3rem;
+        background-color: transparent;
+        border: none;
+    }
+    input[type="color"]::-webkit-color-swatch {
+        border: 0.2rem solid var(--darkGrey);
+        border-radius: 50%;
+    }
+</style>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,21 +1,51 @@
 <template>
-    <div>
-        <h3>testes</h3>
+    <div class="settings main-wrapper">
+        <div class="swatch-container">
+            <div v-for="swatch in swatches" :key="swatch.id" >
+                <Swatches :thisSwatch="swatch" v-on:setTheme="setTheme"></Swatches>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import Swatches from '@/components/Swatches.vue'
+    
     export default {
         props: [  ],
-        components: {  }, 
+        components: { Swatches }, 
         name: 'SettingsView',
         data(){
             return{
-                
+                swatches:[  {   id: 1, 
+                                mainColor: '#0087ff', 
+                                secColor: '#00e1ff', 
+                                enoughTime: '#00d8ff', 
+                                shortTime: '#0097ff', 
+                                noTime: '#006eff'},
+                            {   id: 2, 
+                                mainColor: '#ff8c00', 
+                                secColor: '#fed766', 
+                                enoughTime: '#fcc000', 
+                                shortTime: '#f68608', 
+                                noTime: '#dd242a'},
+                            {   id: 3, 
+                                mainColor: '#268d17', 
+                                secColor: '#5bc328', 
+                                enoughTime: '#77FF94', 
+                                shortTime: '#A1E44D', 
+                                noTime: '#60993E'}]
             }
         },
         methods:{
-        
+            setTheme(swatch){
+                const root = document.querySelector(':root');
+                root.style.setProperty('--blue', swatch.mainColor);
+                root.style.setProperty('--hover', swatch.secColor);
+                root.style.setProperty('--enoughTime', swatch.enoughTime);
+                root.style.setProperty('--shortTime', swatch.shortTime);
+                root.style.setProperty('--noTime', swatch.noTime);
+            },
         },
         computed:{
         
@@ -24,5 +54,12 @@
 </script>
 
 <style scoped>
-
+    .settings{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .swatch-container{
+        width: 28rem;
+    }
 </style>
